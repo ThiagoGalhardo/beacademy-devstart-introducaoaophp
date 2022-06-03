b4f4c56c7354f8e03adc67825ffb817809c05045
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-06-03 20:44:42 -0300

    add exercise02

---
+++ modulo1/exercicio02.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>Variáveis</title>
+</head>
+
+<body>
+
+
+  <?php
+
+$nome = "Thiago";
+$bebida = "café com leite";
+
+
+echo "<h1 class='text-center mt-5 fs-4'>Olá, meu nome é {$nome} e eu gosto de {$bebida} !</h1>";
+
+?>
+
+
+</body>
+
+</html>
