50f98f3ffa2b6ad8ba17f77ae0b88d6580c09218
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-06-03 21:36:17 -0300

    add exercise07

---
+++ modulo1/exercicio07.php
@@ -0,0 +1,17 @@
+<?php
+
+// Operadores lógicos
+
+$numero = 10;
+
+echo $numero == 11;
+
+// == igualdade (valor)
+// === identicidade (valor e tipo)
+// > maior que
+// < menor que
+// >= maior igual que
+// <= menor igual que
+// ! negação
+// != negar a igualdade
+// !== negar identicidade
