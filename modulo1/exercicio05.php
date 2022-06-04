499e6a22a0e2b47faec7a9a40380ff03ea42fa46
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-06-03 21:23:33 -0300

    add exercise05

---
+++ modulo1/exercicio05.php
@@ -0,0 +1,24 @@
+<?php
+//Operadores de incremento/ decremento
+
+
+//Aritméticos
+
+$numero = 10;
+$numero += 5; //Incremento valor de $numero + 5
+$numero++; //Incremento $numero +1
+
+$salario = 1000.00;
+$salarioDaMaria = 2000.00;
+
+$salario = $salario + 200;
+
+$salarioDaMaria = $salarioDaMaria + 200;
+
+$salario += 200; //Incremento valor de $salario + 200
+
+$salarioDaMaria += 200; //Incremento valor de $salarioMaria + 200
+
+$salario -= 200; //Decremento valor de $salario - 200
+
+?>
