95a453bc112a50f74a0d43e5b39ffa5751fd701b
Thiago Galhardo <thiago.galhardo@hotmail.com>
2022-06-03 21:48:29 -0300

    update info

--- modulo1/exercicio07.php
+++ modulo1/exercicio07.php
@@ -13,4 +13,6 @@ echo isset($nome) ? $nome : "nome não definido";
 
 echo "<br>";
 
-echo $nome ?? "nome não definido"; // A partir do php 7
+echo $nome ?? "nome não definido"; // A partir do php 7.4 spread operator
+
+//https://wiki.php.net/rfc/spread_operator_for_array
